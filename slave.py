a3cd3bcbf6bebaddede7e4589fcf809583bc6924
Kevin Ting <kevin@beckys-mbp.lan>
2018-11-23 16:37:51 -0800

    added GET and POST funcitonality along with RgbColor Class

--- slave.py
+++ slave.py
@@ -1,6 +1,30 @@
 from flask import Flask
+from flask import request
+
+class RgbColor:
+    def __init__(self):
+        self.r = 0
+        self.g = 0
+        self.b = 0
+
+    def __str__(self):
+        return f"R: {self.r}, G: {self.g}, B: {self.b}"
+
+
 app = Flask(__name__)
+light = RgbColor()
 
 @app.route("/")
 def hello():
     return "slave root path hello"
+
+@app.route("/color", methods=["GET","POST"])
+def color():
+    if request.method == "GET":
+        return str(light)
+    elif request.method == "POST":
+        light.r = request.json["r"]
+        light.g = request.json["g"]
+        light.b = request.json["b"]
+        return ""
+
